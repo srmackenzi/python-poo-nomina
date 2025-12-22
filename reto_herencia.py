13aaad940a4ec9ccc82fe4f4a6cdfa014b3eab6c
Ibrahim David Butter Saez <saezibrahim@gmail.com>
2025-12-21 20:02:19 -0400

    Initial commmit: Sistema de Nomina con Herencia y Polimorfismo

---
+++ reto_herencia.py
@@ -0,0 +1,106 @@
+# 19/12/2025 mackenziedev.
+# RETO HERENCIA
+# Crear una jerarquia donde existe una clase padre y varias clases hijas que heredan
+# Pero añaden su propia funcionalidad o toque
+# La clase padre sera Empleado, debe tener los atributos:
+# nombre,id,salario base, un metodo mostrar detalles que imprima la informacion del empleado
+# un metodo llamado calculador pago que simplemente retorne el salario base
+# clase hija 1: desarrollador, hereda de empleado, añade lenguaje principal, sobreescribe el 
+# metodo mostrar detalles para que tambien mencione el lenguaje principal
+# clase hija 2: gerente, hereda de empleado, añade departamento, añade el aitrbuto bono
+# sobreescribe el metodo calculador pago para que retorne el salario base mas el bono
+# clase hija 3: freelancer, sin salario base, su metodo calcular pago recibe horas y tarifa por hora
+# -----------------------
+class Empleado:
+    def __init__(self, nombre, id, salario_base):
+        self._nombre = nombre
+        self._id = id
+        self._salario_base = salario_base
+
+    @property
+    def nombre(self): return self._nombre
+    @property
+    def id(self): return self._id
+    @property
+    def salario_base(self): return self._salario_base
+    
+    def mostrar_detalles(self):
+        print(f"\n--- INFORMACION DE {self._nombre} ---")
+        print(f"ID: {self._id} | Salario Base: {self._salario_base}")
+
+    def calcular_pago(self):
+        return self._salario_base
+
+# --- DESARROLLADOR ---
+class Desarrollador(Empleado):
+    def __init__(self, nombre, id, salario_base, lenguaje_principal):
+        super().__init__(nombre, id, salario_base) # Solo lo que pide Empleado
+        self._lenguaje_principal = lenguaje_principal
+
+    def mostrar_detalles(self):
+        super().mostrar_detalles()
+        print(f"Especialidad: Desarrollador {self._lenguaje_principal}")
+
+# --- GERENTE ---
+class Gerente(Empleado):
+    def __init__(self, nombre, id, salario_base, departamento, bono):
+        super().__init__(nombre, id, salario_base)
+        self._departamento = departamento
+        self._bono = bono
+        self._equipo = []
+
+    # Recibimos el objeto completo (no sus datos sueltos)
+    def agregar_empleado(self, empleado_objeto):
+        self._equipo.append(empleado_objeto)
+        print(f"✅ {empleado_objeto.nombre} añadido al equipo de {self._nombre}")
+
+    def mostrar_equipo(self):
+        print(f"\n🚀 MIEMBROS DEL EQUIPO DE {self._nombre.upper()}:")
+        if not self._equipo:
+            print("No hay subordinados asignados.")
+        else:
+            for emp in self._equipo:
+                # Usamos polimorfismo: cada objeto se muestra como sabe
+                emp.mostrar_detalles()
+                print(f"Pago Final: {emp.calcular_pago()}")
+                print("-" * 20)
+
+    def calcular_pago(self):
+        return self._salario_base + self._bono
+
+    def mostrar_detalles(self):
+        super().mostrar_detalles()
+        print(f"Puesto: Gerente de {self._departamento} | Bono: {self._bono}")
+# --- FREELANCER ---
+class Freelancer(Empleado):
+    def __init__(self, nombre, id, horas, tarifa_por_hora):
+        # Un Freelancer no tiene salario_base, pasamos 0 al padre
+        super().__init__(nombre, id, 0)
+        self._horas = horas
+        self._tarifa_por_hora = tarifa_por_hora
+
+    def calcular_pago(self):
+        return self._horas * self._tarifa_por_hora
+
+    def mostrar_detalles(self):
+        super().mostrar_detalles()
+        print(f"Tipo: Freelance | Pago final({self._horas}horas * {self._tarifa_por_hora}): {self.calcular_pago()}")
+
+# --- PRUEBA DEL SISTEMA ---
+if __name__ == "__main__":
+
+    # CREO EL PERSONAL
+    dev1 = Desarrollador("Gabriel", "D001", 1200, "Python")
+    dev2 = Desarrollador("Hamssy", "D002", 1000, "Python")
+    free1 = Freelancer("Phillips", "F001", 15, 100)
+
+    # CREO AL JEFE(GERENTE)
+    boss = Gerente("Ibrahim", "G001", 5000, "Sistemas", 1500)
+
+    # EL JEFE RECIBE LOS OBJETOS(NO LOS DATOS SUELTOS)
+    boss.agregar_empleado(dev1)
+    boss.agregar_empleado(dev2)
+    boss.agregar_empleado(free1)
+
+    # MUESTRO EL EQUIPO PARA TESTEAR
+    boss.mostrar_equipo()
